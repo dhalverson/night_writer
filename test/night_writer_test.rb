73d3019bb460e85febb02dbd770a85bf2757c4eb
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-15 19:00:21 -0600

    Initial commit

---
+++ test/night_writer_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_writer'
+
+class NightWriterTest < Minitest::Test
+
+  def test_it_exists
+    night_writer = NightWriter.new
+  end
+
+
+end
