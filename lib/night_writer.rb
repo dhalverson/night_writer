b8a6f35935975cd8c3b0407b338f9b2393c3fc1c
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-20 20:47:34 -0600

    Add night writer translation method, need to get it to write

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,4 +1,5 @@
 require './lib/alpha'
+require './lib/translator'
 
 class NightWriter
 
@@ -8,12 +9,15 @@ output_file = ARGV[1]
 # Reads from file, removes new lines
 # => "Hello WorldThis project is hard"
 alpha_message = (File.read(input_file)).tr("\n", "")
+translator = Translator.new(alpha_message)
+braille_message = translator.characters_to_braille(alpha_message)
 
+# This should take the message, and write it to the braille file after translating
 
-
+File.write(output_file, braille_message)
+require "pry"; binding.pry
 puts "Created #{ARGV[0]} containing #{File.write(ARGV[1], alpha_message)} characters"
 
-print result
 
 
 
