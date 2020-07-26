e9fde2e83942f7ed16ee250bc32aa4c5fab9da90
Daniel Halverson <daniel.halverson@hotmail.com>
2020-07-26 15:51:07 -0600

    Adds new methods to attempt to write to file, still not working

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,23 +3,33 @@ require './lib/translator'
 
 class NightWriter
 
-input_file = ARGV[0]
-output_file = ARGV[1]
+input = File.open(ARGV[0], "r")
+alpha_message = input.read.strip
+input.close
 
-# Reads from file, removes new lines
-# => "Hello WorldThis project is hard"
-alpha_message = (File.read(input_file)).strip
-
-# translates the message to braille
 translator = Translator.new
 braille_message = translator.whole_braille(alpha_message)
 
-# This should take the message, and write it to the braille file after translating
-output = File.open(output_file, 'w')
-output.write(braille_message)
-output.close
+writer = File.open(ARGV[1], "w")
+writer.write("text goes here")
+writer.close
 
-puts "Created #{output_file} containing #{File.write(output_file, alpha_message)} characters"
+
+# input_file = ARGV[0]
+# output_file = ARGV[1]
+#
+# # Reads from file, removes new lines
+# # => "Hello WorldThis project is hard"
+# alpha_message = File.open(input_file, "r")
+# alpha_message.read
+# require "pry"; binding.pry
+# # translates the message to braille
+# translator = Translator.new
+# braille_message = translator.whole_braille(alpha_message)
+# # This should take the message, and write it to the braille file after translating
+# File.write(output_file, braille_message)
+
+puts "Created #{ARGV[1]} containing #{File.write(ARGV[1], alpha_message)} characters"
 
 
 
