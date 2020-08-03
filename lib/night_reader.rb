713c8ed701a016cba11b048bee3d8d1fe6bbed85
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-02 20:01:39 -0600

    Add translator braille word rows test

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,6 +1,22 @@
+require './lib/alpha'
+require './lib/translator'
+
 class NightReader
 
-  def initialize
-  end
+    input_file = ARGV[0]
+    output_file = ARGV[1]
+
+    input = File.open(input_file, 'r')
+    alpha_message = input.read.strip
+    input.close
+
+    translator = Translator.new
+    alpha_message = translator.
+    # braille_message = translator.display_braille(alpha_message)
+
+    writer = File.open(output_file, 'w') { |file| file.write(alpha_message) }
+
+    puts "Created #{output_file} containing #{alpha_message.chars.count} characters"
+
 
 end
--- lib/translator.rb
+++ lib/translator.rb
@@ -4,18 +4,10 @@ class Translator
     @alpha = Alpha.new
   end
 
-# this is an array of characters from the message
   def create_characters(message)
     message.downcase.chars
   end
 
-  def create_braille(braille)
-    @alpha.braille_chars.invert[braille.scan(/.{2}/)]
-  end
-
-
-
-# this should translate each individual char to it's braille equiv
   def characters_to_braille(message)
     result = []
     create_characters(message).each do |character|
@@ -24,35 +16,29 @@ class Translator
     result
   end
 
-
-
-
-# [["0.", "0.", "0.", "0.", "0."], ["00", ".0", "0.", "0.", ".0"], ["..", "..", "0.", "0.", "0."]]
   def format_braille(message)
     characters_to_braille(message).transpose
   end
 
-# this will be used for if statement, if over 80 chars
-  def message_length(message)
-    message.chars.count
-  end
-
-      # result << format_braille(message)[0].join("").scan(/.{4}/)
   def display_braille(message)
     result = ""
-    if message_length(message) <= 2
-      result << format_braille(message)[0].join("") << "\n"
-      result << format_braille(message)[1].join("") << "\n"
-      result << format_braille(message)[2].join("")
-    else
-      result << format_braille(message)[0].join("") << "\n"
-      result << format_braille(message)[1].join("") << "\n"
-      result << format_braille(message)[2].join("")
-    end
+    result << format_braille(message)[0].join("") << "\n"
+    result << format_braille(message)[1].join("") << "\n"
+    result << format_braille(message)[2].join("")
   end
 
 
 
+
+
+  # this will be used for if statement, if over 80 chars
+    def message_length(message)
+      message.chars.count
+    end
+
+    def braille_to_rows(braille)
+      braille.scan(/.{1,2}/)
+    end
 
 
 
